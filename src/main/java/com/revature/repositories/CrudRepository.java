708e88e8bf0bd37b2b4ec009be682b2488c685bb
tylerrubbert <rubbert_12@live.com>
2020-02-14 12:19:52 -0500

    added dependencies

--- src/main/java/com/revature/repositories/CrudRepository.java
+++ src/main/java/com/revature/repositories/CrudRepository.java
@@ -1,4 +1,13 @@
 package com.revature.repositories;
 
-public interface CrudRepository {
+import java.util.List;
+
+public interface CrudRepository<T> {
+
+    List<T> findAll();
+    T findById(int id);
+    T save(T newOjb);
+    boolean update(T updatedObj);
+    boolean deleteById(int id);
+
 }
